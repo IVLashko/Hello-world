f3c515da313236d8fee0075fed6d92419dc6de23
IVLashko <ivanlasko540@gmail.com>
2025-06-07 14:01:42 +0300

    Add files via upload

---
+++ ПМ С++лб5_1/ПМ С++лб5_1/ПМ С++лб5_1.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+double f1(double x) {
+    return (pow(x, 4) + (2 * x) * 2 + 3);
+}
+double f2(double x) {
+    return (sqrt((2 * x) + log(x)));
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr");
+    double A = 5, B = 8, C = 0.3;
+    double sumf1 = 0.0, sumf2 = 0.0;
+
+    for (double x = A;x <= B;x += C) {
+        sumf1 += f1(x);
+    }
+    for (double x = A;x <= B;x += C) {
+        sumf2 += f2(x);
+    }
+
+    cout << "Сума ряду для f1(x) вiд " << A << " до " << B << " з кроком " << C << " = " << sumf1 << endl;
+    cout << "Сума ряду для f2(x) вiд " << A << " до " << B << " з кроком " << C << " = " << sumf2 << endl;
+}
